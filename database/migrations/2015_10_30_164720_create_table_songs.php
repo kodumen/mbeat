73b46062a9af58aa994cc319c45fe4844124733b
kodumen <r.b.daitan@gmail.com>
2015-10-31 00:59:59 +0800

    Created migrations for songs table

---
+++ database/migrations/2015_10_30_164720_create_table_songs.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableSongs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('title_translit');
+            $table->string('artist');
+            $table->decimal('offset');
+            $table->text('notes_easy');
+            $table->text('notes_medium');
+            $table->text('notes_hard');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('songs');
+    }
+}
