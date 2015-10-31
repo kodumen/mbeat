c45e58a0a7322d7bf8b4996735ec55026f05538e
kodumen <r.b.daitan@gmail.com>
2015-10-31 20:39:15 +0800

    Created initial scripts
    * Added phaser.js
    * Created main.js to define new game
    * Created load_state.js to define loading state
    * Start game when `song_data` was loaded

---
+++ public/mbeat-game/js/load_state.js
@@ -0,0 +1,13 @@
+var loadState = {
+    preload: function () {
+        console.log(song_data);
+    },
+
+    create: function () {
+
+    },
+
+    update: function () {
+
+    }
+};
--- resources/views/game/play.blade.php
+++ resources/views/game/play.blade.php
@@ -5,6 +5,9 @@
     <title>Play</title>
 </head>
 <body>
+<script src="{{asset('/mbeat-game/js/phaser.js')}}"></script>
+<script src="{{asset('/mbeat-game/js/load_state.js')}}"></script>
+<script src="{{asset('/mbeat-game/js/main.js')}}"></script>
 <script>
     var song_data;
     var request = new XMLHttpRequest();
@@ -13,6 +16,7 @@
     request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
             song_data = request.response;
+            game.state.start('Load');
         } else {
             alert('An error occured on the server. Please refresh the page.');
         }
