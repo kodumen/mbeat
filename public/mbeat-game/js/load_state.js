676da509ce2d433178a8620fcf6483551b71c84a
kodumen <r.b.daitan@gmail.com>
2015-11-06 19:42:37 +0800

    Commented out background and music loading

--- public/mbeat-game/js/load_state.js
+++ public/mbeat-game/js/load_state.js
@@ -1,15 +1,15 @@
 var loadState = new Phaser.State();
 
 loadState.preload = function () {
-    this.load.image('background', '/img?src='+ song_data.background);
-    this.load.audio('music', song_data.music);
+    //this.load.image('background', '/img?src='+ song_data.background);
+    //this.load.audio('music', song_data.music);
 };
 
 loadState.create = function () {
-    this.add.image(0, 0, 'background');
-    music = this.add.audio('music');
-    music.mute = false;
-    music.play();
+    //this.add.image(0, 0, 'background');
+    //music = this.add.audio('music');
+    //music.mute = false;
+    //music.play();
 };
 
 loadState.update = function () {
