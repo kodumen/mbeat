2788c22bbc666061490872fbc3e3d3c4653939f9
kodumen <r.b.daitan@gmail.com>
2015-11-08 22:08:05 +0800

    Change sprite texture on key press
    Instead of setTexture, which requires a PIXI.Texture object, use
    loadTexture(), to set the texture by key from the cache.
    
    NOTE: Other keys (F, J, K) do not affect the image yet. Only D works.
    
    * Replaced setTexture() with loadTexture()

--- public/mbeat-game/js/behaviors/KeyBehavior.js
+++ public/mbeat-game/js/behaviors/KeyBehavior.js
@@ -8,16 +8,16 @@
 var KeyBehavior = function (game_object, state) {
     this.game_object = game_object;
     this.state = state;
-    this.default_img = state.cache.getImage(game_object.data.default_image);
-    this.alt_img = state.cache.getImage(game_object.data.alt_image);
+    this.default_img = game_object.data.default_img;
+    this.alt_img = game_object.data.alt_img;
 };
 
 KeyBehavior.prototype.update = function () {
     // Assume that there is a `data` property in the object.
     // `data` is an object with `default_img` and `alt_img` properties.
     if (this.state.input.keyboard.isDown(Phaser.KeyCode.D)) {
-        this.game_object.setTexture(this.alt_img);
+        this.game_object.loadTexture(this.alt_img);
     } else {
-        this.game_object.setTexture(this.default_img);
+        this.game_object.loadTexture(this.default_img);
     }
 };
