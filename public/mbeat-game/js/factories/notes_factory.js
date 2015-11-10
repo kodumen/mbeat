97f2867d1aa3457078b6eeb566a72723b9385f84
kodumen <r.b.daitan@gmail.com>
2015-11-10 23:26:06 +0800

    Destroy notes if offscreen
    But not before they appear onscreen first.

--- public/mbeat-game/js/factories/notes_factory.js
+++ public/mbeat-game/js/factories/notes_factory.js
@@ -58,6 +58,10 @@ Mbeat.factory.note = function (state, x, y, key, time, type) {
 
     note.update = function () {
         this.y += (Mbeat.BEAT_GAP * 87.5 / 60) * (state.time.elapsedMS / 1000);
+
+        if (this.y > game.height) {
+            this.destroy();
+        }
     };
 
     return note;
