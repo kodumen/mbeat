86008b147729aded494ad59e148be1aff5ae338f
kodumen <r.b.daitan@gmail.com>
2015-11-14 14:49:45 +0800

    Created music factory
    Create a Sound object and add it to the world. Play the sound once
    after the offset.

---
+++ public/mbeat-game/js/factories/music_factory.js
@@ -0,0 +1,24 @@
+Mbeat.factory.music = function (state, music_key, offset) {
+    var system = {};
+
+    system.state = state;
+    system.offset = offset;
+    system.music = state.add.audio(music_key);
+    system.timer = 0;
+    system.hasPlayed = false;
+
+    system.update = function () {
+        this.timer += this.state.time.physicsElapsed;
+
+        if (this.timer >= this.offset && !this.hasPlayed) {
+            this.music.mute = false;
+            this.music.play();
+            this.hasPlayed = true;
+        }
+
+        Mbeat.debug.music_timer = this.timer;
+    };
+
+    state.systems.push (system);
+    return system;
+};
